d9f572e10a8b434a46224e6ef7c2cf88e5ad3815
MrinalGupta7890 <mrinalgupta7890@gmail.com>
2024-07-14 13:32:32 +0530

    updated

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -14,11 +14,11 @@ const RestaurantCard = (props) => {
 
     return(
         <div className="res-card" style={styleCard}>
-            <img className="res-logo"
-            src={
-              CDN_URL + resData.info.cloudinaryImageId
-            }
-            alt="Biryani"/>
+            <img 
+            className="res-logo"
+            alt="res-logo"
+            src={CDN_URL + resData.info.cloudinaryImageId}
+            />
             <h3>{name}</h3>
             <h4>{cuisines.join(", ")}</h4>
             <h4>{avgRating} stars</h4>
