c7751f2effc1fc9ba75868303e3461a27c82fe9b
MrinalGupta7890 <mrinalgupta7890@gmail.com>
2024-07-15 20:11:01 +0530

    Updated

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
-import About from "./components/About";
+// import About from "./components/About";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
 import RestaurantMenu from "./components/RestaurantMenu";
@@ -20,6 +20,7 @@ import { createBrowserRouter,RouterProvider,Outlet } from "react-router-dom";
  */
 
 const Grocery = lazy(() => import("./components/Grocery"));
+const About = lazy(() => import("./components/About"));
 
 
 const AppLayout = () => {
@@ -42,7 +43,9 @@ const appRouter = createBrowserRouter([
             },
             {
                 path: "/about",
-                element: <About/>,
+                element: <Suspense fallback={<h1>Loading.....</h1>}>
+                            <About/>
+                        </Suspense>,
             },
             {
                 path: "/contact",
