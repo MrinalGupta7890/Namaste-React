5af72ca90f5eb0add42d7139913000c7a693412c
MrinalGupta7890 <mrinalgupta7890@gmail.com>
2024-07-19 11:43:19 +0530

    Updated

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -13,7 +13,8 @@ const RestaurantCard = (props) => {
     } = resData?.info;
 
     return(
-        <div className="m-4 p-4 w-[240px] rounded-lg" style={{backgroundColor: "#f0f0f0"}}>
+        <div className="m-4 p-4 w-[240px] rounded-lg bg-gray-100 hover:bg-gray-200">
+
             <img 
             className="rounded-lg"
             alt="res-logo"
