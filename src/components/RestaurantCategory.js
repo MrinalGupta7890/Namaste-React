b674493c383feae08859e36d9eaf841f9b1ab834
MrinalGupta7890 <mrinalgupta7890@gmail.com>
2024-07-22 18:39:28 +0530

    Updated

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import ItemList from "./ItemList";
 
 const RestaurantCategory = ({data}) => {
     // console.log(data)
+
+    const [showItems, setShowItems] = useState(false);
+
+    const handleClick = () => {
+        // console.log("clicked");
+        setShowItems(!showItems);
+    }
+
+
     return (
         <div>
             {/**Accordian Header */}
             <div className="w-6/12 mx-auto my-4 bg-gray-50 shadow-lg p-4">
-                <div className="flex justify-between">
+                <div 
+                    className="flex justify-between cursor-pointer" 
+                    onClick={handleClick}>
                     <span className="font-bold text-lg">
                         {data.title} ({data.itemCards.length})
                     </span>
@@ -14,7 +26,7 @@ const RestaurantCategory = ({data}) => {
                 </div>
 
                 {/**Accordian Body */}
-                <ItemList items={data.itemCards}/>
+                {showItems && <ItemList items={data.itemCards}/>}
             </div>
         </div>
     );
--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -40,7 +40,7 @@ const RestaurantMenu = () => {
             </p>
             {/**categories accordian */}
             {categories.map((category) => (
-                <RestaurantCategory data={category?.card?.card} />
+                <RestaurantCategory key={category?.card?.card.title} data={category?.card?.card} />
             ))}
         </div>
     );
