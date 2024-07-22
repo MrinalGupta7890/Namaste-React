cc72d7e70475bcfd8b3e01f1949166d05aab9527
MrinalGupta7890 <mrinalgupta7890@gmail.com>
2024-07-22 17:50:55 +0530

    Updated

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -2,7 +2,15 @@ const ItemList = ({items}) => {
     console.log(items)
     return(
         <div>
-            Category Items
+            {items.map(item => (
+                <div key={item.card?.info?.id} className="p-2 m-2 border-gray-200 border-b-2 text-left">
+                    <div className="py-2">
+                        <span>{item.card.info.name}</span>
+                        <span> - ₹ {item.card.info.price/100}</span>
+                    </div>
+                    <p className="text-xs">{item.card.info.description}</p>
+                </div>   
+            ))}
         </div>
     )
 }
