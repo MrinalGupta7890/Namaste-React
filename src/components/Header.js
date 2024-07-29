67331bfcbb784e9e2e0381c88b6780e5aae92e77
MrinalGupta7890 <mrinalgupta7890@gmail.com>
2024-07-29 18:03:13 +0530

    Updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,62 +3,63 @@ import { LOGO_URL } from "../utils/constants";
 import { Link } from "react-router-dom";
 import useOnlineStatus from "../utils/useOnlineStatus";
 import UserContext from "../utils/UserContext";
+import { useSelector } from "react-redux";
 
 const Header = () => {
-    const[btnNameReact, setbtnNameReact] = useState("Login");
+  const [btnNameReact, setbtnNameReact] = useState("Login");
 
-    // if no dependency array => useEffect is called on every render
-    // if dependency array is empty = [] => useEffect is called on initial render(just once) 
-    // if dependency array is [btnNameReact] => called everytime btnNameReact is updated
-    // useEffect(() => {
-    //     console.log("useEffect called");
-    // },[btnNameReact]);
+  // if no dependency array => useEffect is called on every render
+  // if dependency array is empty = [] => useEffect is called on initial render(just once)
+  // if dependency array is [btnNameReact] => called everytime btnNameReact is updated
+  // useEffect(() => {
+  //     console.log("useEffect called");
+  // },[btnNameReact]);
 
-    const onlineStatus = useOnlineStatus();
+  const onlineStatus = useOnlineStatus();
 
-    const { loggedInUser } = useContext(UserContext);
-    console.log(loggedInUser);
+  const { loggedInUser } = useContext(UserContext);
+  console.log(loggedInUser);
 
+  // Subscribing to the store using a Selector
+  const cartItems = useSelector((store) => store.cart.items);
 
-    return (
-        <div className="flex justify-between bg-pink-100 shadow-lg m-2 sm:bg-yellow-100 lg:bg-green-100">
-            <div className="logo-container">
-                <img className="w-56" src={LOGO_URL}/>
-            </div>
-            <div className="flex items-center">
-                <ul className="flex p-4 m-4">
-                    <li className="px-4">
-                        Online Status: {onlineStatus ?  "✅" : "🔴"}
-                    </li>
-                    <li className="px-4">
-                        <Link to="/">Home</Link>
-                    </li>
-                    <li className="px-4">
-                        <Link to="/about">About Us</Link>
-                    </li>
-                    <li className="px-4">
-                        <Link to="/contact">Contact Us</Link>
-                    </li>
-                    <li className="px-4">
-                        <Link to="/grocery">Grocery</Link>
-                    </li>
-                    <li className="px-4">Cart</li>
-                    <button 
-                        className="login"
-                        onClick={() => {
-                            btnNameReact === "Login" 
-                            ? setbtnNameReact("Logout") 
-                            : setbtnNameReact("Login");
-                        }}
-                    >
-                        {btnNameReact}
-                    </button>
+  return (
+    <div className="flex justify-between bg-pink-100 shadow-lg m-2 sm:bg-yellow-100 lg:bg-green-100">
+      <div className="logo-container">
+        <img className="w-56" src={LOGO_URL} />
+      </div>
+      <div className="flex items-center">
+        <ul className="flex p-4 m-4">
+          <li className="px-4">Online Status: {onlineStatus ? "✅" : "🔴"}</li>
+          <li className="px-4">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="px-4">
+            <Link to="/about">About Us</Link>
+          </li>
+          <li className="px-4">
+            <Link to="/contact">Contact Us</Link>
+          </li>
+          <li className="px-4">
+            <Link to="/grocery">Grocery</Link>
+          </li>
+          <li className="px-4 font-bold text-xl">Cart - ({cartItems.length} items)</li>
+          <button
+            className="login"
+            onClick={() => {
+              btnNameReact === "Login"
+                ? setbtnNameReact("Logout")
+                : setbtnNameReact("Login");
+            }}
+          >
+            {btnNameReact}
+          </button>
 
-                    <li className="px-4 font-bold">{loggedInUser}</li>
-                </ul>
-            </div>
-        </div>
-    );
+          <li className="px-4 font-bold">{loggedInUser}</li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/utils/cartSlice.js
+++ src/utils/cartSlice.js
@@ -3,10 +3,12 @@ import { createSlice } from "@reduxjs/toolkit";
 const cartSlice = createSlice({
   name: "cart",
   initialState: {
-    items: [],
+    items: ["burger","pizza"],
   },
+  // reducers are objects
   reducers: {
     addItem: (state, action) => {
+        // mutating the state here
       state.items.push(action.payload);
     },
     removeItem: (state, action) => {
