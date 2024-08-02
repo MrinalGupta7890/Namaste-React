318d996f484191cbd64a5ff9a10a30119932f30d
MrinalGupta7890 <mrinalgupta7890@gmail.com>
2024-08-02 17:14:28 +0530

    Updated

---
+++ src/components/__tests__/Contact.test.js
@@ -0,0 +1,11 @@
+import { render } from "@testing-library/react"
+import Contact from "../Contact"
+
+
+test("Should load contact us component", () => {
+    render(<Contact/>);
+
+    const heading = screen.getByRole("heading");
+
+    expect(heading).toBeInTheDocument();
+});
