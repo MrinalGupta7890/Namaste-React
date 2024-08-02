8cc38157b36b2cee6986081953563199373d4a04
MrinalGupta7890 <mrinalgupta7890@gmail.com>
2024-08-02 17:40:04 +0530

    Updated

--- README.md
+++ README.md
@@ -119,3 +119,6 @@
   - Configure Parcel Config file to disable default babel transpilation
   - Jest Configuration
   - Install jsdom library
+  - Install @babel/preset-react library - to make JSX work in test cases
+  - Include @babel/preset-react inside my babel config
+  - Install @testing-library/jest-dom library
--- src/components/__tests__/Contact.test.js
+++ src/components/__tests__/Contact.test.js
@@ -1,5 +1,6 @@
-import { render } from "@testing-library/react"
-import Contact from "../Contact"
+import { render, screen } from "@testing-library/react";
+import Contact from "../Contact";
+import "@testing-library/jest-dom";
 
 
 test("Should load contact us component", () => {
@@ -7,5 +8,16 @@ test("Should load contact us component", () => {
 
     const heading = screen.getByRole("heading");
 
+    // Assertion
     expect(heading).toBeInTheDocument();
+});
+
+
+test("Should load button inside contact component", () => {
+    render(<Contact/>);
+
+    const button = screen.getByRole("button");
+
+    // Assertion
+    expect(button).toBeInTheDocument();
 });
